ac9f22e777412f47d3d2d6f02f57b5e51de221c5
TheFirstDon7 <samaigbiriocasey7@gmail.com>
2022-08-18 10:52:51 +0100

    fourth commit

--- index.js
+++ index.js
@@ -7,8 +7,3 @@ hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active")
 })
 
-document.querySelectorAll(".nav-link").forEach(n => n.
-    addEventListener("click", () => {
-        hamburger.classList.remove('active');
-        navMenu.classList.remove('active');
-    }))
